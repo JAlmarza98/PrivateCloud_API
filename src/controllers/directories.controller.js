f0318c080b76bd3b3e571883a4586fc030c49222
JAlmarza98 <j.almarza98@gmail.com>
2022-08-30 22:07:21 +0200

    crear y eliminar directorios del file system

--- src/controllers/directories.controller.js
+++ src/controllers/directories.controller.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 const { decodeJWT } = require('../middlewares/jwt')
 const processPath = require('../middlewares/paths')
 
@@ -36,4 +37,47 @@ const getDirectoryContent = async (req, res) => {
   }
 }
 
-module.exports = { getDirectoryContent }
+const createNewFolder = async (req, res) => {
+  const newFolder = req.body.newFolder
+  try {
+    const token = req.headers.authorization
+    const user = decodeJWT(token).name
+    let dirPath
+    if (req.params.path) {
+      dirPath = processPath(`${user}_${req.params.path}`)
+    } else {
+      dirPath = processPath(user)
+    }
+    fs.mkdir(path.join(dirPath.absolutePath, newFolder), (err) => {
+      if (err) {
+        res.json({
+          success: false,
+          msg: 'No se ha podido crear el directorio'
+        })
+      }
+      res.json({ success: true, msg: 'Nuevo directorio creado' })
+    })
+  } catch (err) {
+    res.json({
+      success: false,
+      msg: 'Ha ocurrido un error inesperado, por favor pongase en contacto con el administrador del servicio'
+    })
+  }
+}
+
+const deleteItem = async (req, res) => {
+  try {
+    const token = req.headers.authorization
+    const user = decodeJWT(token).name
+    const dirPath = processPath(`${user}_${req.params.path}`)
+    fs.rmSync(dirPath.absolutePath, { recursive: true, force: true })
+    res.json({ success: true, msg: 'archivo o directorio eliminado' })
+  } catch (err) {
+    res.json({
+      success: false,
+      msg: 'Ha ocurrido un error inesperado, por favor pongase en contacto con el administrador del servicio'
+    })
+  }
+}
+
+module.exports = { getDirectoryContent, createNewFolder, deleteItem }
--- src/routes/directories.routes.js
+++ src/routes/directories.routes.js
@@ -1,7 +1,11 @@
-const { getDirectoryContent } = require('../controllers/directories.controller')
+const { getDirectoryContent, createNewFolder, deleteItem } = require('../controllers/directories.controller')
 
 const router = require('express').Router()
 
 router.get('/:path?', getDirectoryContent)
+
+router.post('/:path?', createNewFolder)
+
+router.delete('/:path?', deleteItem)
 
 module.exports = router
